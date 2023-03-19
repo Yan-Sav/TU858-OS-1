8887fc8df41b550884b2d161cdc9043cc360e389
Yan-Sav <124152793+Yan-Sav@users.noreply.github.com>
2023-03-19 14:27:02 +0000

    OS assignment work

--- menu.sh
+++ menu.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 PS3="Choose your option: "
-cal=("Sum" "Subt" "Div" "Mult" "Quit")
+cal=("List files" "Show free disk space " "Show system path " "Display command history " "Back up Files" "Exit")
 select i in "${cal[@]}" ; do
     case $i in
-        "Sum")
+        "List files")
             read -p "Enter number 1:" v1
             read -p "Enter number 2:" v2
             echo "Sum of v1 and v2 is: $(($v1+$v2))"
             ;;
-        "Subt")
+        "Show free disk space")
             read -p "Enter number 1:" v1
             read -p "Enter number 2:" v2
             echo "Sum of v1 and v2 is: $(($v1-$v2))"
             ;;
-        "Div")
+        "Show system path")
             read -p "Enter number 1:" v1
             read -p "Enter number 2:" v2
             echo "Sum of v1 and v2 is: $(($v1/$v2))"
             ;;
-        "Mult")
+        "Display command history")
             read -p "Enter number 1:" v1
             read -p "Enter number 2:" v2
             echo "Sum of v1 and v2 is: $(($v1*$v2))"
             ;;
-        "Quit")
-            echo "Quitting the menu"
+        "Back up Files")
+            echo "Backing up files."
+            ;;
+        "Exit")
+            echo "Quitting the menu."
             break
             ;;
         *)
